605b44a8ceca3d81281965aaa94538a0d1d30a95
Maarten Paauw <maartenpaauw@gmail.com>
2016-11-30 11:11:59 +0100

    Begonnen aan schieten

--- assets/js/schieten.js
+++ assets/js/schieten.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
 
+    var kogel  = document.getElementById('kogel'),
+        camera = document.getElementById('camera');
+    
+    document.addEventListener("keypress", schiet, false);
+    
+    function schiet(e) {
+        var keyCode = e.charCode;
+
+        if(keyCode == 46) {
+
+            $(kogel).attr('visible', 'true');
+            $(kogel).attr('visible', 'false');
+        }
+
+        console.log(keyCode)
+    }
+    
 });
