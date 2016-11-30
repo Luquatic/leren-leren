e624deb9a68f0535f0d4e1c88d53f07336dc845a
Maarten Paauw <maartenpaauw@gmail.com>
2016-11-30 10:47:21 +0100

    Bukken werkt

--- assets/js/bukken.js
+++ assets/js/bukken.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
 
+    var camera = document.getElementById('camera');
+
+    document.addEventListener("keydown", buk, false);
+    document.addEventListener("keyup", sta, false);
+
+    function buk(e) {
+        var keyCode = e.keyCode;
+
+        if(keyCode == 67) {
+            $(camera).attr('camera', 'userHeight: 2.8')
+        }
+    }
+
+    function sta(e) {
+        var keyCode = e.keyCode;
+
+        if (keyCode == 67) {
+            $(camera).attr('camera', 'userHeight: 5.6')
+        }
+    }
+
 });
