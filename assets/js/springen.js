93b287ff353b1af9127d012d0373123f1ad25cef
Maarten Paauw <maartenpaauw@gmail.com>
2016-11-30 10:50:46 +0100

    Springen werkt

--- assets/js/springen.js
+++ assets/js/springen.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
 
+    var camera = document.getElementById('camera');
+
+    document.addEventListener("keypress", zweefEven, false);
+
+    function zweefEven(e) {
+        var keyCode = e.keyCode;
+
+        if(keyCode == 32) {
+            $(camera).attr('camera', 'userHeight: 10');
+
+            setTimeout(function () {
+                $(camera).attr('camera', 'userHeight: 5.6');
+            }, 250)
+        }
+    }
 });
