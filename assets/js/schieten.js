1c17ec41f475fed9691c85d6d9419de9109249a0
Maarten Paauw <maartenpaauw@gmail.com>
2016-11-30 11:27:07 +0100

    Schieten

--- assets/js/schieten.js
+++ assets/js/schieten.js
@@ -10,6 +10,11 @@ $(document).ready(function () {
 
         if(keyCode == 46) { 
             $(kogel).attr('visible', 'true');
+            $(kogel).append('<a-animation attribute="position" dur="5000" fill="forwards" to="-0.45 -1 -50"></a-animation>');
+
+            setTimeout(function() {
+                $(kogel).attr('visible', 'false');
+            }, 5000)
         }
 
         console.log(keyCode)
